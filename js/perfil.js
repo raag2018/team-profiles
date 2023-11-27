0b46d8459928527df24a96396228ae6856ca4057
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2023-11-27 11:38:56 -0600

    se agrego la estructura de los archivos html, css, js para perfil

---
+++ js/perfil.js
@@ -0,0 +1,6 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+document.querySelector('#app').innerHTML = `
+
+`;
