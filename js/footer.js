97f799d312d819827428aa9dec7d6ffe6c4c86ae
IDnns <de.acosta@outlook.com>
2023-12-03 21:22:32 -0600

    :zap: Creadores en footer

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ document.querySelector('#footer').innerHTML = `
 <footer class=" padre fixed-bottom bg-body-tertiary text-center text-white text-lg-start mt-5">
 <!-- Copyright -->
 <div class="hijo text-center p-3 " >
-  © 2023 Copyright:
+  © 2023 Copyright: Roberto Antonio Alferes Gómez, Edgard David Ixcoy Chuta, Dennis Antonio Acosta Flores
   <a class="text-body" href="#">Team #1</a>
 </div>
 <!-- Copyright -->
