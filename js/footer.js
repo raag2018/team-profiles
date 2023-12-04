6c2a2c6d7aeb63c5a811c0d96b82b7a881a0f7ee
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2023-12-03 21:32:13 -0600

    modificacion del footer

--- js/footer.js
+++ js/footer.js
@@ -3,8 +3,19 @@ document.querySelector('#footer').innerHTML = `
 <footer class=" padre fixed-bottom bg-body-tertiary text-center text-white text-lg-start mt-5">
 <!-- Copyright -->
 <div class="hijo text-center p-3 " >
-  © 2023 Copyright: Roberto Antonio Alferes Gómez, Edgard David Ixcoy Chuta, Dennis Antonio Acosta Flores
+  <div class='row'>
+    <div class='col-sm-6'>
+    © 2023 Copyright<br/>
+    <label>Roberto Antonio Alferes Gómez</label><br/>
+    </div>
+    <div class='col-sm-6'>
+    <label>Edgard David Ixcoy Chuta</label><br/>
+    <label class=''>Dennis Antonio Acosta Flores</label><br/>
+    </div>
+    <div class='col-sm-12'>
   <a class="text-body" href="#">Team #1</a>
+    </div>
+  </div>
 </div>
 <!-- Copyright -->
 </footer>
